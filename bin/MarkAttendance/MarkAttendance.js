0e79481dcaf348a89d4d0385774d2f4696a5bf39
nastyjack <rishabh.nishad.98@gmail.com>
2020-11-19 12:35:18 +0530

    added catch for errors and responses

--- bin/MarkAttendance/MarkAttendance.js
+++ bin/MarkAttendance/MarkAttendance.js
@@ -3,17 +3,6 @@ const chrome = require("selenium-webdriver/chrome");
 const webdriver = require("selenium-webdriver");
 const Send = require("../../helpers/Email_Utils");
 
-//=========Uncomment for heroku deployed code========//
-// let options = new chrome.Options();
-// options.setChromeBinaryPath(process.env.CHROME_BINARY_PATH);
-// let serviceBuilder = new chrome.ServiceBuilder(process.env.CHROME_DRIVER_PATH);
-
-// //Important for heroku
-// options.addArguments("--headless");
-// options.addArguments("--disable-gpu");
-// options.addArguments("--no-sandbox");
-//===================================================//
-
 const width = 640;
 const height = 480;
 
@@ -24,43 +13,43 @@ module.exports = async function SeleniumScript(action) {
     // Uncomment this to enable browser headless mode. The processing of script will be done in background without visuals
     // new chrome.Options().headless().windowSize({ width, height })
     ()
-    // Uncomment to enable code for heroku
-    // .setChromeService(serviceBuilder)
     .build();
 
   try {
     await driver.get(process.env.GREYTHR_ATTENDANCE_LINK);
-
     await console.log("\n> Starting script, opening URL...");
 
-    await driver.wait(until.elementLocated(By.id("username")), 30000);
+    await driver.wait(until.elementLocated(By.id("username")), 3000);
     await driver
       .findElement({ id: "username" })
       .sendKeys(process.env.GREYTHR_ID);
     await driver
       .findElement({ id: "password" })
       .sendKeys(process.env.GREYTHR_PWD, Key.ENTER);
+    await console.log("\n> Logged In, processing...");
 
-    await console.log("> Logged In, processing...");
-
-    await driver.wait(until.titleIs("Mark Attendance"), 30000);
-    await driver.sleep(2000);
+    await driver.wait(until.titleIs("Mark Attendance"), 3000);
+    await driver.sleep(); //wait for page load to finish
     if (action == "SIGN IN") {
-      await doSignIn(driver);
+      //Uncomment this to allow clicking of Sign-In button for attendance.
+      // await driver.findElement(By.className("btn btn-large btn-success signIn")).click();
+      Send.Mail("Sign IN Success", null);
       await closeScript(driver);
       return "script success";
-    }
-    if (action == "SIGN OUT") {
-      await doSignOut(driver);
+    } else if (action == "SIGN OUT") {
+      //Uncomment this to allow clicking of Sign-Out button for attendance.
+      // await driver.findElement(By.className("btn btn-large btn-success signOut")).click();
+      Send.Mail("Sign OUT Success", null);
       await closeScript(driver);
       return "script success";
-    }
+    } else console.log("\n> Nothing was changed.");
   } catch (error) {
-    console.log("Error occured ", error);
+    if (error.name != "ElementNotInteractableError")
+      console.log("Error occured ", error);
     if (action != undefined && action == "SIGN IN")
       Send.Mail("Sign IN Failed", error.toString());
     else Send.Mail("Sign OUT Failed", error.toString());
-    return error;
+    return error; //throw error at controller which is then caught by catch()
   } finally {
     await driver.quit();
   }
@@ -68,21 +57,8 @@ module.exports = async function SeleniumScript(action) {
 
 async function closeScript(driver) {
   await console.log("\n> Finished Operation, Exiting...");
+  await driver.sleep(); //wait for page load to finish
   await driver.findElement(By.className("empSignOut")).click();
-  await driver.wait(until.titleIs("greytHR IDP"), 30000);
-  await console.log("\n> Script Finished.");
-}
-
-function doSignIn(driver) {
-  //Uncomment this to allow clicking of Sign-In button for attendance.
-  // driver.findElement(By.className("btn btn-large btn-success signIn")).click();
-  Send.Mail("Sign IN Success", null);
-  console.log("Sign IN Success, mail sent");
-}
-
-function doSignOut(driver) {
-  //Uncomment this to allow clicking of Sign-Out button for attendance.
-  // driver.findElement(By.className("btn btn-large btn-success signOut")).click();
-  Send.Mail("Sign OUT Success", null);
-  console.log("Sign OUT Success, mail sent");
+  await driver.wait(until.titleIs("greytHR IDP"), 3000);
+  await console.log("\n> Script Finished.\n ");
 }
--- bin/MarkAttendance/controller.js
+++ bin/MarkAttendance/controller.js
@@ -8,10 +8,16 @@ MarkAttendance.SignIn = async (req, res, next) => {
       if (ScriptResult == "script success")
         res.status(200).send("Sign IN completed");
       else throw ScriptResult;
-    }
+    } else res.status(401).send("Passcode Denied.");
   } catch (error) {
-    console.log("error = ", error);
-    res.status(500).send({ error: error });
+    if ((error.name = "ElementNotInteractableError")) {
+      res
+        .status(202)
+        .send("You are already signed IN for today! Unable to click button.");
+    } else {
+      console.log("script faliure :", error);
+      res.status(500).send({ error: error });
+    }
   }
 };
 
@@ -22,10 +28,16 @@ MarkAttendance.SignOut = async (req, res, next) => {
       if (ScriptResult == "script success")
         res.status(200).send("Sign OUT completed");
       else throw ScriptResult;
-    }
+    } else res.status(401).send("Passcode Denied.");
   } catch (error) {
-    console.log("error = ", error);
-    res.status(500).send({ error: error });
+    if ((error.name = "ElementNotInteractableError")) {
+      res
+        .status(202)
+        .send("You are already signed OUT for today! Unable to click button.");
+    } else {
+      console.log("script faliure :", error);
+      res.status(500).send({ error: error });
+    }
   }
 };
 
