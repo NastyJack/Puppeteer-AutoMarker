58562bb669a005be6186487711f065d097b9d46a
nastyjack <rishabh.nishad.98@gmail.com>
2020-11-20 14:49:37 +0530

    minor fix to operations

--- bin/Operations/controller.js
+++ bin/Operations/controller.js
@@ -2,8 +2,8 @@ let Operations = {};
 
 Operations = (req, res, next) => {
   req.body.passcode == "operations"
-    ? res.send("Hello welcome to operations route")
-    : res.status(500).send("Some error occured");
+    ? res.status(200).send("Hello welcome to operations route")
+    : res.status(500).send("Some error occured " + req.body);
 };
 
 module.exports = Operations;
