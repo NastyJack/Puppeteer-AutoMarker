948b37eba292420f3101bb6a736904b8175e8377
Rishabh Nishad <rishabh@mordorintelligence.com>
2021-12-14 00:05:37 +0530

    fixes to new version of greytHR using shadowRoot at button

--- bin/MarkAttendance/MarkAttendance.js
+++ bin/MarkAttendance/MarkAttendance.js
@@ -1,11 +1,15 @@
 const puppeteer = require("puppeteer");
 
 module.exports = async function AutoMarker(action) {
-  let isSuccess = false;
+  let isSuccess = false,
+    text = null,
+    buttonToClick = `document.querySelector("body > app > ng-component > div > div > div > ghr-home > div > div.page.page-home > div > gt-home-dashboard > div > div:nth-child(2) > gt-component-loader > gt-attendance-info > div > div > div.btn-container.mt-3x.flex.flex-row-reverse.justify-between.ng-star-inserted > gt-button:nth-child(1)").shadowRoot.querySelector("button")`,
+    buttonText = `document.querySelector("body > app > ng-component > div > div > div > ghr-home > div > div.page.page-home > div > gt-home-dashboard > div > div:nth-child(2) > gt-component-loader > gt-attendance-info > div > div > div.btn-container.mt-3x.flex.flex-row-reverse.justify-between.ng-star-inserted > gt-button:nth-child(1)").shadowRoot.querySelector("button > span")`;
   console.log("Inside Puppy Script");
   const browser = await puppeteer.launch({
     //headless: false,
     //  slowMo: 500,
+
     headless: true,
     defaultViewport: null,
     args: ["--incognito", "--no-sandbox", "--single-process", "--no-zygote"],
@@ -22,24 +26,27 @@ module.exports = async function AutoMarker(action) {
 
   if (action == "SIGN IN") {
     console.log("\n > Waiting for button to load...");
-    await page.waitForXPath("//button[contains(., 'Sign In')]");
-    console.log("\n > Signing IN...");
-    const [doSignIn] = await page.$x("//button[contains(., 'Sign In')]");
-    if (doSignIn) {
+    text = (await page.evaluateHandle(buttonText)).asElement();
+    text = await GetProperty(text, `innerHTML`);
+    // console.log("button text element ==", text);
+    const doSignIn = (await page.evaluateHandle(buttonToClick)).asElement();
+    if (text === "Sign In" && doSignIn) {
       await doSignIn.click();
       isSuccess = true;
+      console.log("\n > Signing IN...");
     } else {
       throw "ERROR 'Sign In' Button not found";
     }
   } else if (action == "SIGN OUT") {
     console.log("\n > Waiting for button to load...");
-    await page.waitForXPath("//button[contains(., 'Sign Out')]");
-    console.log("\n > Signing OUT...");
-    const [doSignOut] = await page.$x("//button[contains(., 'Sign Out')]");
-
-    if (doSignOut) {
+    text = (await page.evaluateHandle(buttonText)).asElement();
+    text = await GetProperty(text, `innerHTML`);
+    // console.log("button text element ==", text);
+    const doSignOut = (await page.evaluateHandle(buttonToClick)).asElement();
+    if (text === "Sign Out" && doSignOut) {
       await doSignOut.click();
       isSuccess = true;
+      console.log("\n > Signing OUT...");
     } else {
       throw "ERROR 'Sign Out' Button not found";
     }
@@ -51,4 +58,8 @@ module.exports = async function AutoMarker(action) {
   console.log(" > Finished Script");
   if (isSuccess) return "script success";
   else throw "Script faliure occured";
+};
+
+GetProperty = async (element = ElementHandle, property = string) => {
+  return await (await element.getProperty(property)).jsonValue();
 };
